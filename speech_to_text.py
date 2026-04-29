b799ef4592802f8fe2a83379f06b62dbe98c1e0c
Anshuman Upadhyay <anshuman0042@gmail.com>
2026-04-29 15:47:51 +0530

    Made quick workarounds

--- speech_to_text.py
+++ speech_to_text.py
@@ -1,6 +1,18 @@
+# The segment section of each clip holds the details about the chunks(smallparts)
+import os
 import whisper
-model = whisper.load_model("base")
-result = model.transcribe(audio = "ex-audio/8_Forms-and-input-tags-in-HTML-.mp3",
+import json
+model = whisper.load_model("large-v2")
+filepath = "/home/anshuman/D_Drive/Coding/TeachingAssistant/ex-audio/3_realme-Buds-T500-Pro-Review-Bluetooth-6-_Media_g67kuqZyM_g_002_720p.mp4.mp3"
+result = model.transcribe(audio = filepath,
                           language = "hi",
                           task = "translate")
-print(result["text"])
+# print(result["text"])
+chunks = []
+for segment in result["segments"]:
+    chunks.append({ "start" : segment["start"],
+                   "end" : segment["end"],
+                   "text" : segment["text"], })
+with open("output.json","w") as f:
+    json.dump(chunks,f,indent=4)
+    
