6488534bbeb4cd708e9df2b43dbd7d6858a722ed
Anshuman Upadhyay <anshuman0042@gmail.com>
2026-04-30 20:54:30 +0530

    This was a big one made major upgrade hope i wont regret doing these

--- chunk_maker.py
+++ chunk_maker.py
@@ -2,26 +2,109 @@
 import os
 import whisper
 import json
+from deep_translator import GoogleTranslator
+
 model = whisper.load_model("large-v2")
-audios = os.listdir("ex-audio")
-for audio in audios:
-    number= audio.split("_")[0]
-    title = audio.split("_")[1][:-20]
-    print(number,title)
-    result = model.transcribe(audio = f"ex-audio/{audio}",
-                          language = "hi",
-                          task = "translate",
-                          word_timestamps= False)
+
+def translate_to_english(text,source_lang):
+    if source_lang == "en":
+        return text
+    try :
+        return GoogleTranslator(
+            source="auto", 
+            target="en",
+            ).translate(text)
+    except Exception as e :
+        print("Translation Error ",e)
+        return text
+
+def merge_segement(segments,lang,number,title,min_length = 50):
     chunks = []
-    for seg in result["segments"]:
-        chunks.append({
-                "number" : number,
+    current_group = []
+    current_text = ""
+
+    for seg in segments :
+        text = seg.get("text","").strip()
+
+        #skip truly empty chunks
+        if not text :
+            continue
+
+        #accumulate 
+        current_group.append(seg)
+        current_text += " " + text 
+
+        # once a considerable length is reached make the final chunks
+        if len(current_text.strip()) >= min_length:
+            #translate
+            translated = translate_to_english(current_text.strip(),lang)
+            chunks.append({
+                "number":number,
                 "title" : title,
-                "start" : seg["start"],
-                "end" : seg["end"],
-                "text" : seg["text"]
+                "start" : current_group[0]["start"],
+                "end" : current_group[-1]["end"],
+                "original_text" : current_text.strip(),
+                "translated_text" :translated
             })
-    chunks_with_metadata = {"chunks" : chunks,
-                            "text" : result["text"]}
-    with open(f"Json/{audio}.json","w") as f:
-        json.dump(chunks_with_metadata,f,indent=4)
+        # reset the chunks for next
+        current_text= ""
+        current_group = []
+
+    #handle leftover 
+    if current_group and current_text.strip():
+        chunks.append({
+            "number":number,
+            "title" : title,
+            "start": current_group[0]["start"],
+            "end": current_group[-1]["end"],
+            "original_text" : current_text.strip(),
+            "translated_text" :translated
+        })
+    return chunks
+
+def process_audio(filepath,output,number,title):
+    print(f"\n Processing : {filepath}")
+
+    result= model.transcribe(
+        audio = filepath,
+        task = "transcribe",
+        language = None
+    )
+
+    lang = result.get("language")
+    chunks = merge_segement(
+        result["segments"],
+        lang,
+        number,
+        title
+        )
+
+    with open(output,"w") as f:
+        json.dump({
+            "number":number,
+            "title" : title,
+            "language" : lang,
+            "chunks" : chunks,
+            "full_text" : result["text"]
+        },f,indent = 4,ensure_ascii=False)
+
+        print("Saved : ",output)
+
+
+input_folder = "ex-audio"
+output_folder ="Json"
+
+os.makedirs(output_folder,exist_ok=True)
+
+for audio_file in os.listdir(input_folder):
+    
+    parts = audio_file.split("__")
+    number = parts[0] if len(parts) > 0 else "unknown"
+    title = parts[1] if len(parts) > 1 else "unknown"
+    print(f"Processing : \nFile Number : {number} \nName: {title}")
+
+    file_path = os.path.join(input_folder,audio_file)
+    output_file = os.path.join(output_folder,f"{audio_file}.json")
+
+    process_audio(file_path,output_file,number,title)
+print("Done")
--- read_chunks.py
+++ read_chunks.py
@@ -1,46 +1,105 @@
 #make vector embeddings out of the extracted texts and make them into pandas dataframe 
-import requests
+# https://github.com/ollama/ollama/blob/main/docs/api.md#generate-embeddings
 import os
 import json
+import math
+import requests
 import pandas as pd
+
+# replace the Nan values 
+def clean_embeddings(e):
+    return [0 if (isinstance(x,float) and math.isnan(x)) else x for x in e]
+
 def create_embeddings(text_list):
-    # https://github.com/ollama/ollama/blob/main/docs/api.md#generate-embeddings
-    r = requests.post(
-        "http://localhost:11434/api/embed",
-        json={
-            "model": "bge-m3",
-            "input": text_list
-        }
-    )
-
-    embedding = r.json()
-
+    try :
+        r = requests.post(
+            "http://localhost:11434/api/embed",
+            json={
+                "model": "bge-m3",
+                "input": text_list
+            },
+            timeout= 60
+        )
+        r.raise_for_status()
+        embedding = r.json()
+    except Exception as e :
+        print(f"Request Failed : {e}")
+        return None
+    
     if "embeddings" not in embedding:
-        print(embedding)
-        return []
+        print(f"API Error : {embedding}")
+        return None
 
     return embedding["embeddings"]
+
 #list all the jsons
 jsons = os.listdir("Json")
 
 #storing all embeddings
 my_dict=[]
 chunk_id= 0
+counter = 0 
+batch_size = 20
+
 
 #get all the json files extracted from audio
 for json_files in jsons:
+
     #open each individual file 
     with open(f"Json/{json_files}") as f:
         content = json.load(f)
-    #get a list of all the text in each json's embeddings
-    embeddings = create_embeddings([x["text"] for x in content["chunks"]])
-    print(f"Creating embeddings for {json_files}")
+
+    print(f"\n{counter}. Creating embeddings for {json_files} \n")
+    counter+=1
+    
+    texts = []
+    valid_indices = []
+
     #get each individual chunks out of the json file 
-    for i,chunk in enumerate(content["chunks"]):
+    for i,chunk in enumerate(content.get("chunks",[])):
+        text = chunk.get("translated_text","") or chunk.get("original_text","")
+        text = text.strip()
+        if text :
+            texts.append(text)
+            valid_indices.append(i)
+            
+    # skip if nothing valid
+    if not texts :
+        print(f"Skipping {json_files} : (No valid Text)")
+        continue
+
+    #Batch Embeddings
+    all_embeddings = []
+    for i in range(0,len(texts),batch_size):
+        batch = texts[i:i+batch_size]
+        batch_embeddings = create_embeddings(batch)
+
+        if batch_embeddings is None:
+            print(f"Skipping {json_files} due to batch error")
+            break
+        all_embeddings.extend(batch_embeddings)
+
+    if all_embeddings is None or len(all_embeddings) != len(texts) :
+        print(f"Skipping {json_files} cause : Embedding error")
+        continue
+    embeddings = all_embeddings
+ 
+    
+    # Map embeddings back
+    for idx,emb in zip(valid_indices,embeddings):
+        chunk = content["chunks"][idx]
         chunk["chunk_id"] = chunk_id
-        chunk["embedding"] = embeddings[i]
-        chunk_id+=1
+        chunk["embedding"] = clean_embeddings(emb)
+        chunk_id +=1
         my_dict.append(chunk)
-    break
-df = pd.DataFrame.from_records(my_dict)
-print(df)
+    
+
+#Build Dataframe
+if not my_dict:
+    print("No Embeddings Created")
+else:
+    df = pd.DataFrame.from_records(my_dict)
+    print("\nSample data:")
+    print(df.head())
+    print(f"\n Total Rows : {len(df)}")
+
